54ac2997155e5ad494ec11054cfbd050211b5fbe
TorsteinEide <taurstein@gmail.com>
2022-09-22 11:32:22 +0200

    Initialized firebase
    Added dependencies and code to main() method to initialize firebase

--- stattrack/lib/main.dart
+++ stattrack/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:stattrack/pages/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
