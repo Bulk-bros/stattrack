b46f6b0cad609b78b87d6cc70c37427c1cfe7ba7
jKm00 <joakied@stud.ntnu.no>
2022-09-26 22:35:14 +0200

    nav bar in log page

--- stattrack/lib/pages/log_page.dart
+++ stattrack/lib/pages/log_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stattrack/components/CustomAppBar.dart';
+import 'package:stattrack/styles/font_styles.dart';
 import 'package:stattrack/styles/palette.dart';
 
 class LogPage extends StatefulWidget {
@@ -10,6 +11,8 @@ class LogPage extends StatefulWidget {
 }
 
 class _LogPageState extends State<LogPage> {
+  int activeNavItem = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +34,95 @@ class _LogPageState extends State<LogPage> {
             ),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: _buildBody(),
+      ),
+    );
+  }
+
+  /// Returns the body of the log page
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        _buildNav(),
+        const Text('yoyoyo'),
+      ],
+    );
+  }
+
+  /// Returns a nav widget
+  Widget _buildNav() {
+    return Material(
+      color: Colors.white,
+      elevation: 1.5,
+      borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            _navItem(
+              'Daily',
+              () => setState(() {
+                activeNavItem = 0;
+              }),
+              activeNavItem == 0,
+            ),
+            _navItem(
+              'Weekly',
+              () => setState(() {
+                activeNavItem = 1;
+              }),
+              activeNavItem == 1,
+            ),
+            _navItem(
+              'monthly',
+              () => setState(() {
+                activeNavItem = 2;
+              }),
+              activeNavItem == 2,
+            ),
+            _navItem(
+              'Yearly',
+              () => setState(() {
+                activeNavItem = 3;
+              }),
+              activeNavItem == 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Returns a nav item
+  ///
+  /// [label] the label displayed in the item
+  /// [callback] the callback function called when the item is pressed
+  /// [active] a boolean describing if the item is currently active or
+  /// not. Active items has a change in style
+  Widget _navItem(String label, VoidCallback callback, bool active) {
+    return TextButton(
+      onPressed: callback,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: active ? Palette.accent[400] : Colors.transparent,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 20.0,
+          ),
+          child: Text(
+            label,
+            style: TextStyle(
+              color: active ? Colors.white : Colors.black87,
+            ),
+          ),
+        ),
       ),
     );
   }
