4d6260a4bfdc6f397f5b018d3ef9ea37ff0f3e3a
Joakim <joakied@stud.ntnu.no>
2022-09-27 15:31:57 +0200

    removed unused import

--- stattrack/lib/pages/sign_in_page.dart
+++ stattrack/lib/pages/sign_in_page.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 import 'package:stattrack/components/buttons/auth_button.dart';
 import 'package:stattrack/components/logos/logo.dart';
