6c736c8c391698e1aab8c0a73d50cd6e32fdef31
TorsteinEide <taurstein@gmail.com>
2022-09-27 13:45:52 +0200

    Added firebase authentication

--- stattrack/lib/main.dart
+++ stattrack/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:stattrack/pages/sign_in_page.dart';
 import 'package:stattrack/styles/palette.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
