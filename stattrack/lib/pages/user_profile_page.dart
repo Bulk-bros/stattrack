19efe9eba3531ed80faf31fc8289047b81d767f2
TorsteinEide <taurstein@gmail.com>
2022-10-06 15:36:39 +0200

    Completed layout with static information
    made widgets more abstract to be used other places

--- stattrack/lib/pages/user_profile_page.dart
+++ stattrack/lib/pages/user_profile_page.dart
@@ -8,9 +8,22 @@ import 'package:stattrack/main.dart';
 import 'package:stattrack/styles/font_styles.dart';
 import 'package:stattrack/styles/palette.dart';
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'dart:math' as math;
 
-class UserProfilePage extends StatelessWidget {
+enum NavButtons {
+  macros,
+  meals,
+}
+
+class UserProfilePage extends StatefulWidget {
   const UserProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _UserProfilePageState createState() => _UserProfilePageState();
+}
+
+class _UserProfilePageState extends State<UserProfilePage> {
+  NavButtons activeButton = NavButtons.macros;
 
   @override
   Widget build(BuildContext context) {
@@ -28,21 +41,49 @@ class UserProfilePage extends StatelessWidget {
         ColumnSuper(
           innerDistance: -55,
           children: [
-            _buildUserInformation("Jenny Nilsen", "23", "62kg", "173cm"),
+            _buildBaseContainer(
+                _buildUserInformation("Jenny Nilsen", "23", "62kg", "173cm")),
             spacing,
             Padding(
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  _buildStatbox("Calories", "1236", 230),
+                  _buildCard(
+                      _buildProfilePageMainStatContent(
+                          "Calories", "GRAPH HERE"),
+                      230),
                   spacing,
-                  _buildStatbox("Proteins", "83g"),
+                  _buildCard(
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _statDetails("Proteins", "83g", FontStyles.fsTitle3,
+                            FontStyles.fsTitle1)
+                      ],
+                    ),
+                  ),
                   spacing,
-                  _buildStatbox("Carbs", "340g"),
+                  _buildCard(
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _statDetails("Carbs", "340g", FontStyles.fsTitle3,
+                            FontStyles.fsTitle1)
+                      ],
+                    ),
+                  ),
                   spacing,
-                  _buildStatbox("Fat", "27g")
+                  _buildCard(
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _statDetails("Fat", "27g", FontStyles.fsTitle3,
+                            FontStyles.fsTitle1)
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -52,95 +93,177 @@ class UserProfilePage extends StatelessWidget {
     );
   }
 
-// move this to component folder
-  Widget _buildStatbox(String categoryText, String amountText,
-      [double size = 100]) {
-    return Container(
-      height: size,
-      child: Text(categoryText),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black.withOpacity(0.10),
-              spreadRadius: 5,
-              blurRadius: 10,
-              offset: const Offset(0, 5))
-        ],
-      ),
+  Widget _buildProfilePageMainStatContent(String text, String amountText) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text,
+          style: const TextStyle(fontSize: FontStyles.fsTitle3),
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            amountText,
+            style: const TextStyle(fontSize: FontStyles.fsTitle1),
+          ),
+        )
+      ],
     );
+  }
+
+// move this to component folder
+  Widget _buildCard(Widget content, [double size = 100]) {
+    return Container(
+        padding: const EdgeInsets.only(left: 20),
+        height: size,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.10),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(0, 5))
+          ],
+        ),
+        child: content);
   }
 
   Widget _buildUserInformation(
       String name, String age, String weight, String height,
       [DecorationImage image = const DecorationImage(
           image: AssetImage("assets/images/eddyboy.jpeg"))]) {
-    return Container(
-        height: 320,
-        width: 1000,
-        decoration: BoxDecoration(
-          color: Palette.accent[400],
-          borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.10),
-                spreadRadius: 5,
-                blurRadius: 10,
-                offset: Offset(0, 5))
+    return ColumnSuper(
+      outerDistance: -10,
+      children: [
+        const SizedBox(
+          height: 55,
+        ),
+        RowSuper(
+          fill: true,
+          children: [
+            _encaseProfilePicture(image),
+            WrapSuper(
+              wrapFit: WrapFit.divided,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(
+                          fontSize: FontStyles.fsTitle1,
+                          color: Colors.white,
+                          fontWeight: FontStyles.fwTitle),
+                    ),
+                    const SizedBox(height: 25),
+                    SizedBox(
+                      width: 190,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _statDetails("age", age, FontStyles.fsBodySmall,
+                              FontStyles.fsBody, Colors.white),
+                          _statDetails(
+                              "weight",
+                              weight,
+                              FontStyles.fsBodySmall,
+                              FontStyles.fsBody,
+                              Colors.white,
+                              const Icon(
+                                Icons.play_arrow,
+                                color: Colors.white,
+                                size: 16,
+                              )),
+                          _statDetails("height", height, FontStyles.fsBodySmall,
+                              FontStyles.fsBody, Colors.white),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30)
+                  ],
+                )
+              ],
+            ),
           ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: RowSuper(
-            fill: true,
-            children: [
-              _encaseProfilePicture(image),
-              WrapSuper(
-                wrapFit: WrapFit.proportional,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        style: const TextStyle(
-                            fontSize: FontStyles.fsTitle1,
-                            color: Colors.white,
-                            fontWeight: FontStyles.fwTitle),
-                      ),
-                      const SizedBox(height: 15),
-                      SizedBox(
-                        width: 190,
-                        child: WrapSuper(
-                          wrapFit: WrapFit.divided,
-                          children: [
-                            _statDetails("age", age, FontStyles.fsBodySmall,
-                                FontStyles.fsBody, Colors.white),
-                            _statDetails(
-                                "weight",
-                                weight,
-                                FontStyles.fsBodySmall,
-                                FontStyles.fsBody,
-                                Colors.white),
-                            _statDetails(
-                                "height",
-                                height,
-                                FontStyles.fsBodySmall,
-                                FontStyles.fsBody,
-                                Colors.white),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 30)
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
-        ));
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          children: [
+            Container(
+              width: 160,
+              alignment: Alignment.centerLeft,
+              child: _userInformationTextButton(
+                  "Todays macros", activeButton == NavButtons.macros, () {
+                setState(() {
+                  activeButton = NavButtons.macros;
+                });
+              }),
+            ),
+            Container(
+              width: 160,
+              alignment: Alignment.centerLeft,
+              child: _userInformationTextButton(
+                  "Todays meals", activeButton == NavButtons.meals, () {
+                setState(() {
+                  activeButton = NavButtons.meals;
+                });
+              }),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  //Base Container
+  //Takes a widget parameter which is the body to be displayed on top
+  //Widget makes sure the body has a padding
+  Widget _buildBaseContainer(Widget body) {
+    return Container(
+      height: 320,
+      width: 1000,
+      decoration: BoxDecoration(
+        color: Palette.accent[400],
+        borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: body,
+      ),
+    );
+  }
+
+  Widget _userInformationTextButton(
+      String text, bool highlight, VoidCallback callback) {
+    FontWeight weight = FontStyles.fw400;
+    TextDecoration decoration = TextDecoration.none; // Standard weight
+    if (highlight) {
+      weight = FontStyles.fw600;
+      decoration = TextDecoration.underline;
+    }
+    return TextButton(
+      onPressed: callback,
+      style: TextButton.styleFrom(
+        primary: Colors.white,
+        padding: const EdgeInsets.all(0),
+        splashFactory: NoSplash.splashFactory,
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+            fontWeight: weight,
+            decoration: decoration,
+            decorationThickness: 2,
+            fontSize: FontStyles.fs500),
+      ),
+    );
   }
 
   Widget _encaseProfilePicture(DecorationImage image) {
@@ -154,17 +277,28 @@ class UserProfilePage extends StatelessWidget {
   Widget _statDetails(String categoryText, String amountText,
       [double catergoryTextSize = FontStyles.fsBodySmall,
       double amountTextSize = FontStyles.fsBody,
-      Color color = Colors.black]) {
+      Color color = Colors.black,
+      Icon? icon]) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(categoryText,
             style: TextStyle(fontSize: catergoryTextSize, color: color)),
-        Text(amountText,
-            style: TextStyle(
-                fontSize: amountTextSize,
-                fontWeight: FontStyles.fw600,
-                color: color)),
+        Row(
+          children: [
+            Text(
+              amountText,
+              style: TextStyle(
+                  fontSize: amountTextSize,
+                  fontWeight: FontStyles.fw600,
+                  color: color),
+            ),
+            Transform.rotate(
+              angle: -90 * math.pi / 180,
+              child: icon ?? Text(""),
+            )
+          ],
+        )
       ],
     );
   }
