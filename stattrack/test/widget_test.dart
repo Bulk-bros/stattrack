a7c2873e1701e0df0322109b4650d41ca78904b6
Eduard <77648731+falniir@users.noreply.github.com>
2022-11-08 23:37:21 +0100

    Added Widget tests for Sign In Page

--- stattrack/test/widget_test.dart
+++ stattrack/test/widget_test.dart
@@ -7,22 +7,62 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stattrack/main.dart';
+import 'package:stattrack/components/buttons/auth_button.dart';
 
 void main() {
-  // TODO: Implement tests
   testWidgets('Signin page', (WidgetTester tester) async {
-    expect(true, true);
-    //   Build our app and trigger a frame.
-    //   await tester.pumpWidget(const MyApp());
+    //TODO: Verify that callback returns an user object
 
-    //   // Verify we're on login page
-    //   expect(find.text('Sign in'), findsOneWidget);
-    //   expect(find.text('Home page'), findsNothing);
+    /// Testing Email Sign in Button
+    const authButtonEmail = AuthButton(
+      label: 'Email',
+      iconPath: 'assets/icons/envelope-solid.svg',
+      iconAlt: 'Mail Icon',
+    );
+    await tester.pumpWidget(const MaterialApp(home: authButtonEmail));
+    final buttonFinderEmail = find.byType(AuthButton);
+    expect(buttonFinderEmail, findsOneWidget);
 
-    //   // Check for buttons
-    //   expect(find.widgetWithText(ElevatedButton, 'Facebook'), findsOneWidget);
-    //   expect(find.widgetWithText(ElevatedButton, 'Google'), findsOneWidget);
-    //   expect(find.widgetWithText(ElevatedButton, 'Email'), findsOneWidget);
+    /// Testing Facebook Sign In Button
+    const authButtonFacebook = AuthButton(
+      label: 'Facebook',
+      iconPath: 'assets/icons/square-facebook.svg',
+      iconAlt: 'Facebook Logo',
+    );
+    await tester.pumpWidget(const MaterialApp(home: authButtonFacebook));
+    final buttonFinderFacebook = find.byType(AuthButton);
+    expect(buttonFinderFacebook, findsOneWidget);
+
+    /// Testing Google Sign In Button
+    const authButtonGoogle = AuthButton(
+      label: 'Google',
+      iconPath: 'assets/icons/google.svg',
+      iconAlt: 'Google Logo',
+    );
+    await tester.pumpWidget(const MaterialApp(home: authButtonGoogle));
+    final buttonFinderGoogle = find.byType(AuthButton);
+    expect(buttonFinderGoogle, findsOneWidget);
+
+    /// Testing Sign Up TextButton bellow the buttons
+    /// Broken due to missing widget implementation
+    /// The following assertion was thrown building _InkResponseStateWidget(gestures: [tap], mouseCursor:
+    ///ButtonStyleButton_MouseCursor, clipped to BoxShape.rectangle, dirty, state:
+    ///_InkResponseState#e716b):
+    ///No Directionality widget found.
+    ///_InkResponseStateWidget widgets require a Directionality widget ancestor.
+    ///The specific widget that could not find a Directionality ancestor was:
+    //StatefulElement#20bf0(DEFUNCT)
+
+    //var pressed = false;
+
+    // var authSignUpText = TextButton(
+    //     onPressed: () => pressed = true,
+    //     child: const Text("Don't have an account? Sign up here"));
+
+    // await tester.pumpWidget(authSignUpText);
+    // final buttonFinderSignUp = find.byType(TextButton);
+    // expect(buttonFinderSignUp, findsOneWidget);
+    // await tester.tap(buttonFinderSignUp);
+    // expect(pressed, true);
   });
 }
