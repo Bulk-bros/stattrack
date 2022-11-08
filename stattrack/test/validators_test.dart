695c3611dc29d3499a6627530fa80c8c11d93d02
Eduard <77648731+falniir@users.noreply.github.com>
2022-11-08 15:00:40 +0100

    indentation

--- stattrack/test/validators_test.dart
+++ stattrack/test/validators_test.dart
@@ -20,15 +20,19 @@ void main() {
   test('Valid Email', () {
     expect(Validator.isValidEmail("flutter@google.com"), true);
   });
+
   test('Invalid Email', () {
     expect(Validator.isValidEmail("flutter@google"), false);
   });
+
   test('Valid Password', () {
     expect(Validator.isValidPassword("&1Sgjs2gfsGJ_78"), true);
   });
+
   test('Valid Password, No Symbols', () {
     expect(Validator.isValidPassword("1Sgjs2gfsGJ78"), false);
   });
+
   test('Invalid Password', () {
     expect(Validator.isValidPassword("123jfuhsd4567"), false);
   });
