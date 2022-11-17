38333b6b05331a6c1e449a0dacca51905fa37eb1
Eduard <77648731+falniir@users.noreply.github.com>
2022-11-17 13:23:01 +0100

    added docs

--- stattrack/lib/pages/nav_wrapper.dart
+++ stattrack/lib/pages/nav_wrapper.dart
@@ -4,6 +4,12 @@ import 'package:stattrack/pages/log_page.dart';
 import 'package:stattrack/pages/user_profile_page.dart';
 import 'package:stattrack/utils/nav_button_options.dart';
 
+/// The main page that contains the navigation bar
+/// and the pages that are navigated to
+///
+/// It's used as a canvas to draw the pages on top of and renders the
+/// navigation bar at the bottom of the screen while the pages are
+/// rendered on top of it
 class NavWrapper extends StatefulWidget {
   const NavWrapper({super.key});
 
@@ -29,6 +35,8 @@ class _NavWrapperState extends State<NavWrapper> {
     );
   }
 
+  /// Handles the event when one item from the navigation bar
+  /// is pressed
   void _handleNavChange(String button) {
     switch (button) {
       case NavButtonOptions.profile:
